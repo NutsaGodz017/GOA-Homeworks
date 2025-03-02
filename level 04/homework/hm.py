91f87af18fe37cdd686518214c781a8639cdacc6
NutsaGodz017 <goderidze.nutsa17@gmail.com>
2025-03-02 14:08:23 +0400

    initial commit

---
+++ level 04/homework/hm.py
@@ -0,0 +1,38 @@
+mother = "Sofio"
+father = "Temuri"
+sister = "Mariam"
+sister2 = "Kira"
+dog = "Tobias"
+
+print("my mother is" " " + mother + " " + "my dad is" + " " + father)
+print("my sisters are" " " + sister + ' ' "and" ' ' + sister2)
+
+ 
+ #gamokleba
+print(10-5)
+print(15-5)
+print(100-50)
+print(16-6)
+print(60-10)
+
+#mimateba
+print(5+15)
+print(100+900)
+print(90+10)
+print(1.5+1.5)
+print(4.5+5.5)
+
+#gamravleba
+print(10*5)
+print(5*9)
+print(5*4)
+print(6*9)
+print(4*4)
+
+#gayofa
+print(10/5)
+print(100/2)
+print(60/5)
+print(600/300)
+print(900/3)
+
---
+++ level 06/classwork/cw.py
@@ -0,0 +1,22 @@
+#შექმენით mybalance-ის ცვლადი, შემდეგ გაზარდეთ ამ ცვლადის მნიშვნელობა 2-თ უბრალო ფორმით, შემდეგ ისევ გაზარდეთ მნიშვნელობა 3-ით ოღონდ გამოიყენეთ assignment operator, ამის შემდეგ თქვენი სურვილისამებრ შეცვალეთ ცვლადის მნიშვნელობა იგივე assignment operator-ის საშვალებით, საბოლოოდ დაბეჭდეთ balance. კოდი ახსენით კომენატრებით, ჩამოწერეთ ყველა საუკეტესო პრაქტიკა და მიუწერეთ განმარტებები
+my_ballance = 100
+
+My_ballance = my_ballance + 50
+
+MY_ballance = My_ballance*5
+
+MY_Ballance = MY_ballance/10
+
+print(MY_Ballance)
+
+#meore qlasvorki
+
+hobby = "volleyball"
+activity = "playing"
+print(hobby + activity)
+#stringis daweris shekrebis dros isini sityvierad ertmanets emateba da gverdigverd ewereba
+
+Tuesday = 1.5
+thursday = 1.5
+print(Tuesday + thursday)
+#integeris shekrebisas ki isini matematikurad ikribeba
